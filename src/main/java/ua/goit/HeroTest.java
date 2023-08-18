c6e5385858265809b130a49abbeec00f96a2d144
Anton Vasylevskyi <vasilevskiy@gmail.com>
2023-08-19 00:01:48 +0300

    Added Task 2

--- src/main/java/ua/goit/HeroTest.java
+++ src/main/java/ua/goit/HeroTest.java
@@ -1,24 +1,17 @@
 package ua.goit;
 
-class HeroTest {
-    public static void main(String[] args) {
-        Hero hero = new Hero();
-
-        //Expect Paratrooper
-        System.out.println(hero.getName());
-
-        //Expect 100
-        System.out.println(hero.getHp());
-    }
-}
-
 class Hero {
     private String name;
     private int hp;
 
     public Hero() {
-        this.name = "Paratrooper";
-        this.hp = 100;
+        name = "Paratrooper";
+        hp = 100;
+    }
+
+    public Hero(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
     }
 
 
@@ -29,3 +22,25 @@ class Hero {
         return hp;
     }
 }
+
+
+class HeroTest {
+    public static void main(String[] args) {
+/*         Hero hero = new Hero();
+
+        Task 1  //Expect Paratrooper
+        System.out.println(hero.getName());
+
+        //Expect 100
+        System.out.println(hero.getHp());*/
+
+        Hero hero = new Hero("Stranger", 50);
+
+        //Expect Stranger
+        System.out.println(hero.getName());
+
+        //Expect 50
+        System.out.println(hero.getHp());
+    }
+}
+
