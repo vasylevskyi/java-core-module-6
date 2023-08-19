0f40ecb653e6e43117e7c16ef32ccac7c976b26c
Anton Vasylevskyi <vasilevskiy@gmail.com>
2023-08-19 23:34:16 +0300

    Added Task 6

---
+++ src/main/java/ua/goit/Bullet.java
@@ -0,0 +1,15 @@
+package ua.goit;
+
+public class Bullet {
+    {
+        System.out.println("Bullet created!");
+    }
+}
+
+
+class BulletTest {
+    public static void main(String[] args) {
+        new Bullet();
+        new Bullet();
+    }
+}
